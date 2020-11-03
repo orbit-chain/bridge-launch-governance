42968c013cf0cca6316fab8e4742e264476fe1fe
jayb <56820630+jaybbbb@users.noreply.github.com>
2020-11-03 13:15:48 +0900

    Update settings.js

--- ozys-ethvault/settings.js
+++ ozys-ethvault/settings.js
@@ -64,7 +64,7 @@ module.exports = {
     
     VALIDATOR_MONITOR: {
         Ozys: {
-            Endpoint: "https://bridge-en.orbitchain:7743/v1/validator/report",
+            Endpoint: "https://bridge-en.orbitchain.io:7743/v1/validator/report",
             Interval: 60 * 1000,
         },
     },
