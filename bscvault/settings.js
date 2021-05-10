f93282667f4a30b6e6b75c4904352d7895c354ff
JayB <jayb@ozys.net>
2021-05-10 16:26:28 +0900

    one more time

--- bscvault/settings.js
+++ bscvault/settings.js
@@ -3,9 +3,9 @@ module.exports = {
     chainList: [
         'klaytn-v2',
         'orbit',
-    	'bsc',
-    	'heco',
-    	'matic',
+        'bsc',
+        'heco',
+        'matic',
     ],
 
     // Bridge Addresses
@@ -13,9 +13,9 @@ module.exports = {
         OrbitHubContract: "0xb5680a55d627c52de992e3ea52a86f19da475399",
         KlaytnBridgeContract: "0x1af95905bb0042803f90e36d79d13aea6cd58969",
         OrbitBridgeContract: "0x77a49649964a186Fd2b8754758c39c9438a6E9aB",
-    	BscBridgeContract: "0x89c527764f03BCb7dC469707B23b79C1D7Beb780",
-    	HecoBridgeContract: "0xE7688F64e96A733EaDdCb5850392347e67Bb197f",
-    	MaticBridgeContract: "0x1Fc5A2cE72c71563E6EFC1fc35F326D4CCd23B93",
+        BscBridgeContract: "0x89c527764f03BCb7dC469707B23b79C1D7Beb780",
+        HecoBridgeContract: "0xE7688F64e96A733EaDdCb5850392347e67Bb197f",
+        MaticBridgeContract: "0x1Fc5A2cE72c71563E6EFC1fc35F326D4CCd23B93",
         MessageMultiSigWallet: {
             Hub: "0x5059725ed52970725d882B7c66f613577BacaEB2",
             Klaytn: "0x5059725ed52970725d882B7c66f613577BacaEB2",
@@ -26,18 +26,18 @@ module.exports = {
         },
         Bsc: {
             VaultContract: "0x89c527764f03BCb7dC469707B23b79C1D7Beb780",
-	        MessageMultiSigWallet: "0xf2C5a817cc8FFaAB4122f2cE27AB8486DFeAb09F",
+            MessageMultiSigWallet: "0xf2C5a817cc8FFaAB4122f2cE27AB8486DFeAb09F",
             BscAdminContract: "0x8D5DcEab358979101dC96A62e08296269F6BD1bd",
         },
         Klay: {
             KlaytnMinterContract: "0xB0a83941058b109Bd0543fa26d22eFb8a2D0f431",
             MessageMultiSigWallet: "0x937936FF183102Dfb1609D5dbFbC50201f92c744",
         },
-    	Orbit: {
+        Orbit: {
             OrbitMinterContract: "0xd4EC00c84f01361F36D907E061EA652eE50572AF",
             MessageMultiSigWallet: "0x5059725ed52970725d882B7c66f613577BacaEB2",
         },
-    	Heco: {
+        Heco: {
             HecoMinterContract: "0xf2C5a817cc8FFaAB4122f2cE27AB8486DFeAb09F",
             MessageMultiSigWallet: "0xE7688F64e96A733EaDdCb5850392347e67Bb197f",
             HecoAdminContract: "0x89c527764f03BCb7dC469707B23b79C1D7Beb780",
@@ -58,10 +58,10 @@ module.exports = {
     // Node Endpoints
     Endpoints : {
         Orbit: {
-    	    rpc : "https://bridge-en.orbitchain.io:7443",
+            rpc : "https://bridge-en.orbitchain.io:7443",
             socket: "wss://bridge-en.orbitchain.io:7444",
         },
-    	Klaytn: {
+        Klaytn: {
             isKas: true,
             Kas: {
                 // KAS Default
@@ -71,8 +71,8 @@ module.exports = {
                 // Your Credential
                 accessKeyId: "",
                 secretAccessKey: ""
-    	    }
-    	},
+            }
+        },
         Heco: {
             rpc : "https://http-mainnet-node.huobichain.com",
             socket: "wss://ws-mainnet.hecochain.com",
@@ -100,6 +100,6 @@ module.exports = {
         Ozys: {
             Endpoint: "https://bridge-en.orbitchain.io:7743/bscvault/v1/validator/report",
             Interval: 60 * 1000,
-    	},
+        },
     },
 }
