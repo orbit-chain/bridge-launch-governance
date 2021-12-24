3d0d0a3cb235526f2f003d2fc583f36fc7751f57
JayB <jayb@ozys.net>
2021-12-24 12:32:26 +0900

    update xrp minter info

--- xrpvault/settings.js
+++ xrpvault/settings.js
@@ -4,6 +4,16 @@ module.exports = {
         'klaytn-v2',
         'xrp',
     	'orbit',
+        'avax',
+        'bsc',
+        'celo',
+        'heco',
+        'fantom',
+        'harmony',
+        'matic',
+        'moonriver',
+        'oec',
+        'xdai',
     ],
 
     // Bridge Addresses
@@ -18,6 +28,16 @@ module.exports = {
             Klaytn: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
             Orbit: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
             Xrp: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
+            Avax: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
+            Bsc: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
+            Celo: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
+            Fantom: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
+            Harmony: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
+            Heco: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
+            Matic: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
+            Moonriver: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
+            Oec: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
+            Xdai: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
         },
         Klay: {
             KlaytnMinterContract: "0x917655B6C27A3D831b4193CE18ECe6bfcC806BF8",
@@ -26,6 +46,56 @@ module.exports = {
         Orbit: {
             OrbitMinterContract: "0xe288716122c53e8df43884cb06b85cffdaa57546",
             MessageMultiSigWallet: "0x1e9f03b3bdcb33bfec6ef504356bcbc72392e3c5",
+        },
+        Avax: {
+            AvaxMinterContract: "0xE38ca00A6FD34B793012Bb9c1471Adc4E98386cF",
+            MessageMultiSigWallet: "0xfa50391705D2FA7ac47Dd211B78378825bc763e6",
+            AvaxAdminContract: "0x009071058740276327A393B084eC447b8F0Fc6Ae",
+        },
+        Bsc: {
+            BscMinterContract: "0xE38ca00A6FD34B793012Bb9c1471Adc4E98386cF",
+            MessageMultiSigWallet: "0xfa50391705D2FA7ac47Dd211B78378825bc763e6",
+            BscAdminContract: "0x009071058740276327A393B084eC447b8F0Fc6Ae",
+        },
+        Celo: {
+            CeloMinterContract: "0xE38ca00A6FD34B793012Bb9c1471Adc4E98386cF",
+            MessageMultiSigWallet: "0xfa50391705D2FA7ac47Dd211B78378825bc763e6",
+            CeloAdminContract: "0x009071058740276327A393B084eC447b8F0Fc6Ae",
+        },
+        Fantom: {
+            FantomMinterContract: "0xE38ca00A6FD34B793012Bb9c1471Adc4E98386cF",
+            MessageMultiSigWallet: "0xfa50391705D2FA7ac47Dd211B78378825bc763e6",
+            FantomAdminContract: "0x009071058740276327A393B084eC447b8F0Fc6Ae",
+        },
+        Harmony: {
+            HarmonyMinterContract: "0xE38ca00A6FD34B793012Bb9c1471Adc4E98386cF",
+            MessageMultiSigWallet: "0xfa50391705D2FA7ac47Dd211B78378825bc763e6",
+            HarmonyAdminContract: "0x009071058740276327A393B084eC447b8F0Fc6Ae",
+        },
+        Heco: {
+            HecoMinterContract: "0xE38ca00A6FD34B793012Bb9c1471Adc4E98386cF",
+            MessageMultiSigWallet: "0xfa50391705D2FA7ac47Dd211B78378825bc763e6",
+            HecoAdminContract: "0x009071058740276327A393B084eC447b8F0Fc6Ae",
+        },
+        Matic: {
+            MaticMinterContract: "0xE38ca00A6FD34B793012Bb9c1471Adc4E98386cF",
+            MessageMultiSigWallet: "0xfa50391705D2FA7ac47Dd211B78378825bc763e6",
+            MaticAdminContract: "0x009071058740276327A393B084eC447b8F0Fc6Ae",
+        },
+        Moonriver: {
+            MoonriverMinterContract: "0xE38ca00A6FD34B793012Bb9c1471Adc4E98386cF",
+            MessageMultiSigWallet: "0xfa50391705D2FA7ac47Dd211B78378825bc763e6",
+            MoonriverAdminContract: "0x009071058740276327A393B084eC447b8F0Fc6Ae",
+        },
+        Oec: {
+            OecMinterContract: "0xE38ca00A6FD34B793012Bb9c1471Adc4E98386cF",
+            MessageMultiSigWallet: "0xfa50391705D2FA7ac47Dd211B78378825bc763e6",
+            OecAdminContract: "0x009071058740276327A393B084eC447b8F0Fc6Ae",
+        },
+        Xdai: {
+            XdaiMinterContract: "0xE38ca00A6FD34B793012Bb9c1471Adc4E98386cF",
+            MessageMultiSigWallet: "0xfa50391705D2FA7ac47Dd211B78378825bc763e6",
+            XdaiAdminContract: "0x009071058740276327A393B084eC447b8F0Fc6Ae",
         },
         Governance: {
             Chain: "XRP",
@@ -62,7 +132,47 @@ module.exports = {
         Xrp: {
             rpc: "https://s1.ripple.com:51234",
             socket: "wss://s1.ripple.com:443",
-        }
+        },
+        Avax: {
+            rpc: "https://api.avax.network/ext/bc/C/rpc",
+            socket: "https://api.avax.network/ext/bc/C/ws",
+        },
+        Bsc: {
+            rpc : "https://bsc-dataseed.binance.org",
+            socket: "wss://bsc-ws-node.nariox.org:443",
+        },
+        Celo: {
+            rpc: "https://forno.celo.org",
+            socket: "wss://forno.celo.org/ws",
+        },
+        Fantom: {
+            rpc: "https://rpc.ftm.tools",
+            socket: "wss://wsapi.fantom.network",
+        },
+        Harmony: {
+            rpc: "https://api.harmony.one",
+            socket: "wss://ws.s0.t.hmny.io",
+        },
+        Heco: {
+            rpc : "https://http-mainnet-node.huobichain.com",
+            socket: "wss://ws-mainnet.hecochain.com",
+        },
+        Matic: {
+            rpc : "https://rpc-mainnet.maticvigil.com",
+            socket: "wss://rpc-mainnet.matic.quiknode.pro",
+        },
+        Moonriver: {
+            rpc: "https://moonriver.api.onfinality.io/public",
+            socket: "wss://moonriver.api.onfinality.io/public-ws",
+        },
+        Oec: {
+            rpc: "https://exchainrpc.okex.org",
+            socket: "wss://exchainws.okex.org:8443",
+        },
+        Xdai: {
+            rpc: "https://rpc.xdaichain.com",
+            socket: "ws://xdai.poanetwork.dev:8546",
+        },
     },
 
     DEBUG: true,
